6861bbf5bfa938cb6584ce7e4fdf30f2f0e4dfcf
Keith H. Bova <keithhbova@gmail.com>
2020-11-20 18:34:05 -0700

    added functions to get path of player_init directory to parse players.txt

--- shadowRunBase/main.cpp
+++ shadowRunBase/main.cpp
@@ -1,6 +1,23 @@
 #include "utils.h"
 #include "players.h"
 
+std::string GetCurrentWorkingDir( void ) {
+  char buff[FILENAME_MAX];
+  GetCurrentDir( buff, FILENAME_MAX );
+  std::string current_working_dir(buff);
+  return current_working_dir;
+}
+
+std::string getPathOfPlayerInitFile(const std::string & fileName){
+	std::stringstream ss;
+	std::string buildDirectory = GetCurrentWorkingDir();
+	for(int i = 0; i < 5; i++){
+		buildDirectory.pop_back();
+	}
+	ss << buildDirectory << "player_init/" << fileName;
+	return ss.str();
+}
+
 void writeNewPlayer(const std::string & fileName, const Player & player){
 	std::ofstream newPlayer;
 	newPlayer.open(fileName);
@@ -28,8 +45,8 @@ int main(){
 	Player dante("Dante");
 	//std::cout << "Player:\t" << dante.toString() << std::endl;
 
-
-	std::cout << loadPlayerFromTxt("/home/keith/Desktop/shadowrunstuff/shadowRun/player_init/Chalk.txt") << std::endl;
+	std::string chalkPath = getPathOfPlayerInitFile("Chalk.txt");
+	std::cout << loadPlayerFromTxt(chalkPath) << std::endl;
 	//writeNewPlayer("player_init//dante.txt", dante);
 
 	return 0;
--- shadowRunBase/utils.h
+++ shadowRunBase/utils.h
@@ -1,13 +1,21 @@
 #pragma once
 #ifndef UTILS_H
 #define UTILS_H
-#include <stdio.h>
-#include <iostream>
 #include <fstream>
 #include "yaml-cpp/yaml.h"
 #include <string>
 #include <sstream>
-
+#include <stdio.h> 
+// #define WINDOWS  
+#ifdef WINDOWS
+#include <direct.h>
+#define GetCurrentDir _getcwd
+#else
+#include <unistd.h>
+#define GetCurrentDir getcwd
+#endif
+#include <iostream>
+ 
 
 
 
